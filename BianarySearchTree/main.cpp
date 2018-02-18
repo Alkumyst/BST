4d7378502ef5a9768b047b8cde3c4ddb5d5b3396
James Hudspeth <unit001@ymail.com>
2018-02-18 15:56:38 -0700

    Initial Commit

---
+++ BianarySearchTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BianarySearchTree
+//
+//  Created by James D. Hudspeth on 2/18/18.
+//  Copyright © 2018 James D. Hudspeth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
